16f1e983cea13999030572f06eeacb1a434987b9
Thomas Bernhart <t.bernhart@docuteam.ch>
2020-04-02 18:59:10 +0200

    FCREPO-3193: Add Dockerfile for fcrepo-webapp 6.0.0

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM tomcat:9-jdk11-openjdk
+
+ENV TOMCAT_USERS_FILE=$CATALINA_HOME/conf/tomcat-users.xml \
+    FEDORA_ADMIN_PASSWORD=fedoraAdmin \
+    FCREPO_OCFL_STAGING_DIR=/var/lib/fcrepo/staging \
+    FCREPO_OCFL_STORAGE_ROOT_DIR=/var/lib/fcrepo/ocfl \
+    FCREPO_OCFL_WORK_DIR=/var/lib/fcrepo/work \
+    FCREPO_ACTIVEMQ_DIRECTORY=/var/lib/fcrepo/activemq \
+    FCREPO_AUDIT_CONTAINER=/audit
+
+# Unclear if still used:
+# fcrepo.home=/var/lib/fcrepo
+# com.arjuna.ats.arjuna.common.ObjectStoreEnvironmentBean.default.objectStoreDir=arjuna.common.object.store
+# com.arjuna.ats.arjuna.objectstore.objectStoreDir=arjuna.object.store
+
+# Add webapp, scripts and config files: 
+COPY tomcat/conf/* $CATALINA_HOME/conf/
+COPY tomcat/bin/* $CATALINA_HOME/bin/setenv.sh
+COPY fcrepo-webapp.war /tmp/fcrepo-webapp.war
+
+# Set permissions and unpack webapp
+RUN chmod 744 $CATALINA_HOME/bin/setenv.sh \
+    && chmod 600 $CATALINA_HOME/conf/tomcat-users.xml \
+    && mkdir $CATALINA_HOME/webapps/fcrepo \
+    && unzip /tmp/fcrepo-webapp.war -d $CATALINA_HOME/webapps/fcrepo \
+    && rm /tmp/fcrepo-webapp.war
---
+++ tomcat/bin/setenv.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Add system properties for env variables defined in docker image:
+CATALINA_OPTS="$CATALINA_OPTS \
+-Dfile.encoding=UTF-8 \
+-Dorg.apache.tomcat.util.digester.PROPERTY_SOURCE=org.apache.tomcat.util.digester.EnvironmentPropertySource \
+-Dfcrepo.ocfl.staging.dir=$FCREPO_OCFL_STAGING_DIR \
+-Dfcrepo.ocfl.storage.root.dir=$FCREPO_OCFL_STORAGE_ROOT_DIR \
+-Dfcrepo.ocfl.work.dir=$FCREPO_OCFL_WORK_DIR \
+-Dfcrepo.audit.container=$FCREPO_AUDIT_CONTAINER"
+
+# Only add these system properties, if the env variable is defined:
+
+if [ ! -z "$FCREPO_SPRING_CONFIGURATION" ] ; then
+  CATALINA_OPTS="$CATALINA_OPTS -Dfcrepo.spring.configuration=$FCREPO_SPRING_CONFIGURATION"
+fi
+
+if [ ! -z "$FCREPO_ACTIVEMQ_CONFIGURATION" ] ; then
+  CATALINA_OPTS="$CATALINA_OPTS -Dfcrepo.activemq.configuration=$FCREPO_ACTIVEMQ_CONFIGURATION"
+else
+  CATALINA_OPTS="$CATALINA_OPTS -Dfcrepo.activemq.directory=$FCREPO_ACTIVEMQ_DIRECTORY"
+fi
+
+if [ ! -z "$FCREPO_AUTH_WEBAC_AUTHORIZATION" ] ; then
+  CATALINA_OPTS="$CATALINA_OPTS -Dfcrepo.activemq.configuration=$FCREPO_AUTH_WEBAC_AUTHORIZATION"
+fi
+
+if [ ! -z "$FCREPO_SPRING_AUDIT_CONFIGURATION" ] ; then
+  CATALINA_OPTS="$CATALINA_OPTS -Dfcrepo.spring.audit.configuration=$FCREPO_SPRING_AUDIT_CONFIGURATION"
+fi
+
+if [ ! -z "$LOGBACK_CONFIGURATIONFILE" ] ; then
+  CATALINA_OPTS="$CATALINA_OPTS -Dlogback.configurationFile=$LOGBACK_CONFIGURATIONFILE"
+fi
+
+if [ ! -z "$FCREPO_VELOCITY_RUNTIME_LOG" ] ; then
+  CATALINA_OPTS="$CATALINA_OPTS -Dfcrepo.velocity.runtime.log=$FCREPO_VELOCITY_RUNTIME_LOG"
+fi

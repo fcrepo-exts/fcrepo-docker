ffd8b3f5a131d63dec1d0bf2d5866b61c7a05172
Dan Field <dan.field@lyrasis.org>
2024-09-05 18:44:56 +0100

    Fcrepo 3912 (#22)
    * adding test for empty and latest docker tags
    
    * adding test for empty and latest docker tags
    
    * moving test to start of build script at Jareds request
    
    * removing invalidated comment

--- build-and-push-to-dockerhub.sh
+++ build-and-push-to-dockerhub.sh
@@ -6,6 +6,16 @@ set -e
 FCREPO_WEBAPP_FULL_PATH="$1"
 FCREPO_WEBAPP_FILE=$(basename "$FCREPO_WEBAPP_FULL_PATH")
 
+for docker_tag in "${@:2}"
+do
+    # Check that valid tag is supplied. If not, fail
+    if [ "latest" == "${docker_tag}" ] || [ -z "${docker_tag}" ]
+    then
+        echo "Building latest or empty tags is no longer supported"
+        exit 1
+    fi
+done
+
 if [ -d ./webapp ]; then
     rm -r ./webapp
 fi
@@ -22,13 +32,6 @@ platforms=linux/arm64,linux/amd64
 for docker_tag in "${@:2}"
 do
     echo "Building and pushing $docker_tag ..."
-    if [ "latest" == $docker_tag ]
-    then
-        docker buildx build --platform ${platforms} --push  -t fcrepo/fcrepo .
-	
-    else
- 	docker buildx build --platform ${platforms} --push  -t fcrepo/fcrepo:$docker_tag .
-    fi
-
+    docker buildx build --platform ${platforms} --push  -t fcrepo/fcrepo:$docker_tag .
     echo "Build and push complete for $docker_tag"
 done
